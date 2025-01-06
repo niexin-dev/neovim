e828002dc283b3710b8471da662ea689cd9ebc9a
Niexin.Liu <383655168@qq.com>
2025-01-06 11:47:06 +0800

    增加nvim-lspconfig用于配置lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,52 +1,65 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        keys = {
-            -- 设置查看头/源文件
-            { "<F5>", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-        },
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig['clangd'].setup(
-                {
-                    servers = {
-                        -- Ensure mason installs the server
-                        clangd = {
-                            root_dir = function(fname)
-                                return require("lspconfig.util").root_pattern(
-                                    "Makefile",
-                                    "configure.ac",
-                                    "configure.in",
-                                    "config.h.in",
-                                    "meson.build",
-                                    "meson_options.txt",
-                                    "build.ninja"
-                                )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-                                    fname
-                                ) or require("lspconfig.util").find_git_ancestor(fname)
-                            end,
-                            capabilities = {
-                                offsetEncoding = { "utf-16" },
-                            },
-                            cmd = {
-                                "clangd",
-                                "--background-index",
-                                "--clang-tidy",
-                                "--header-insertion=iwyu",
-                                "--completion-style=detailed",
-                                "--function-arg-placeholders",
-                                "--fallback-style=llvm",
-                            },
-                            init_options = {
-                                usePlaceholders = true,
-                                completeUnimported = true,
-                                clangdFileStatus = true,
-                            },
-                        },
-                    },
+    "neovim/nvim-lspconfig",
 
-                }
-            )
-        end
+    keys = {
+        -- 设置查看头/源文件
+        { "<F5>", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
     },
+
+    -- example using `opts` for defining servers
+    opts = {
+        servers = {
+            clangd = {
+                on_attach = function()
+                    vim.lsp.inlay_hint.enable(true)
+                end,
+                -- Ensure mason installs the server
+                root_dir = function(fname)
+                    return require("lspconfig.util").root_pattern(
+                        "Makefile",
+                        "configure.ac",
+                        "configure.in",
+                        "config.h.in",
+                        "meson.build",
+                        "meson_options.txt",
+                        "build.ninja"
+                    )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+                        fname
+                    ) or require("lspconfig.util").find_git_ancestor(fname)
+                end,
+                capabilities = {
+                    offsetEncoding = { "utf-16" },
+                },
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=iwyu",
+                    "--completion-style=detailed",
+                    "--function-arg-placeholders",
+                    "--fallback-style=llvm",
+                },
+                init_options = {
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    clangdFileStatus = true,
+                },
+                format = {
+                    enable = true,
+                    style = "file",
+                },
+            },
+            lua_ls = {},
+        }
+    },
+
+    config = function(_, opts)
+        local lspconfig = require('lspconfig')
+        for server, config in pairs(opts.servers) do
+            -- passing config.capabilities to blink.cmp merges with the capabilities in your
+            -- `opts[server].capabilities, if you've defined it
+            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
+    end
 }
