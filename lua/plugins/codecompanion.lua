257a8a82d0cb4534abd931f354d8c02a7019046d
Niexin.Liu <383655168@qq.com>
2025-03-19 09:03:13 +0800

    feat: 添加 codecompanion 插件配置
    - 添加了 `codecompanion.nvim` 插件配置文件。
    - 配置了依赖项：`plenary.nvim` 和 `nvim-treesitter`。
    - 设置语言选项为 "Chinese"。

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,11 @@
+return {
+    "olimorris/codecompanion.nvim",
+    config = true,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+        language = "Chinese",
+    },
+}
