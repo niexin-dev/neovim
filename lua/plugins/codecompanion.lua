c877593643250668bd2645c4d841d11f8ca85d5d
Niexin.Liu <383655168@qq.com>
2025-03-28 16:21:49 +0800

    feat: 添加生成提交信息的 prompt_library 配置
    在 codecompanion 插件中新增了一个名为 "Generate a Commit Message" 的 prompt_library 配置。该配置使用了 "chat" 策略，并包含了详细的选项和提示，用于生成符合 Conventional Commit 规范的提交信息。

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -9,5 +9,38 @@ return {
         opts = {
             language = "Chinese",
         },
+        prompt_library = {
+            ["Generate a Commit Message"] = {
+                strategy = "chat",
+                description = "Generate a commit message",
+                opts = {
+                    index = 10,
+                    is_default = true,
+                    is_slash_cmd = true,
+                    short_name = "commit",
+                    auto_submit = true,
+                },
+                prompts = {
+                    {
+                        role = "user",
+                        content = function()
+                            return fmt(
+                                [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
+
+```diff
+%s
+```
+]],
+                                vim.fn.system("git diff --no-ext-diff --staged")
+                            )
+                        end,
+                        opts = {
+                            contains_code = true,
+                        },
+                    },
+                },
+            },
+
+        },
     },
 }
