8889cab0824d5878b30a15a2ac49de268f33a724
Niexin.Liu <383655168@qq.com>
2025-05-06 17:57:23 +0800

    feat(keymaps): 优化 j 和 k 键的屏幕行移动
    此提交将 `j` 键映射为 `gj` (向下移动一行屏幕行)，并将 `k` 键映射为 `gk` (向上移动一行屏幕行)。
    这使得在 Neovim 中使用 `j` 和 `k` 键进行垂直移动时，可以按屏幕行而不是实际的文本行进行移动，从而提高浏览代码的效率。
    
    `noremap = true` 禁用递归映射，防止映射链的出现。
    `silent = true` 表示执行映射时不显示消息，保持界面简洁。

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,11 @@ vim.keymap.set("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>")
 
 -- 在当前目录下创建新文件
 vim.keymap.set("n", "<leader>en", ":new <C-R>=expand(\"%:p:h\") . \"/\" <CR>")
+
+-- 将 k 映射为 gk (向上移动一行屏幕行)
+-- noremap = true 表示禁用递归映射，防止映射链的出现
+-- silent = true 表示执行映射时不显示消息
+vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
+-- 将 j 映射为 gj (向下移动一行屏幕行)
+vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
+
