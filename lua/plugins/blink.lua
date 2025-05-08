7e8ab22cab7c95d532526c1932e81211d2f5fec9
Niexin.Liu <383655168@qq.com>
2025-05-08 08:59:58 +0800

    feat(blink): 增强补全源，加入 codecompanion
    在 blink 插件的默认补全源中新增 `codecompanion`，以提供更全面的代码补全支持。

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -38,7 +38,7 @@ return {
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer', 'codeium' },
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'cmdline', 'omni', 'codeium', 'codecompanion' },
             -- default = { 'lsp', 'path', 'snippets', 'buffer', 'cmdline', 'copilot' },
             -- providers = {
             --     copilot = {
