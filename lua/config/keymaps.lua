2ce6de85b16ff6ebbffd9c117c5ebfee5b2a1696
Niexin.Liu <383655168@qq.com>
2025-05-12 17:38:54 +0800

    feat(keymaps): 扩展 j/k 映射到可视模式
    允许在可视模式下使用 j/k 映射到 gj/gk，提升在可视模式下的移动效率。

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,7 +19,7 @@ vim.keymap.set("n", "<leader>en", ":new <C-R>=expand(\"%:p:h\") . \"/\" <CR>")
 -- 将 k 映射为 gk (向上移动一行屏幕行)
 -- noremap = true 表示禁用递归映射，防止映射链的出现
 -- silent = true 表示执行映射时不显示消息
-vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, 'k', 'gk', { noremap = true, silent = true })
 -- 将 j 映射为 gj (向下移动一行屏幕行)
-vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, 'j', 'gj', { noremap = true, silent = true })
 
