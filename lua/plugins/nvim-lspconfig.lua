a13ad5495a0e9d861d5967eecc3e95e3f28776e0
Niexin.Liu <383655168@qq.com>
2025-05-18 17:43:02 +0800

    feat(lspconfig): 修复 clangd 中文注释问题
    修复了在中文注释行下方插入新行时，clangd 报 "Change's rangeLength (1) doesn't match the computed range length (5)" 错误的问题。
    
    原因为 clangd 默认的 offset_encoding 与 Neovim 不同，导致计算 rangeLength 错误。
    
    通过设置 offset_encoding 为 'utf-8'，与 Neovim 保持一致，解决了该问题。

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -21,6 +21,8 @@ return {
         })
         vim.lsp.config['clangd'] = {
             cmd = { 'clangd' },
+            -- 修复在中文注释行下方插入新行导致的Change's rangeLength (1) doesn't match the computed range length (5)错误，造成error: -32602: trying to get AST for non-added document
+            offset_encoding = 'utf-8',
             root_markers = { 'compile_commands.json', '.git' },
             vim.lsp.inlay_hint.enable(true)
         }
