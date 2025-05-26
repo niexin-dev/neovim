6d696bb3467529aa4e80d54f7effa8943c25faf6
Niexin.Liu <383655168@qq.com>
2025-05-26 09:36:45 +0800

    feat: 添加 vim-bookmarks 插件并移除 marks.nvim
    此提交完成了以下操作：
    - 添加了 vim-bookmarks 插件，用于在 Neovim 中管理书签。
    - 配置了书签的符号、高亮组和高亮行。
    - 移除了不再使用的 marks.nvim 插件。
    
    BREAKING CHANGE: 移除了 marks.nvim 插件，如果用户依赖此插件，需要手动安装 vim-bookmarks 作为替代。

---
+++ lua/plugins/vim-bookmarks.lua
@@ -0,0 +1,23 @@
+return {
+    'MattesGroeger/vim-bookmarks',
+    name = 'bookmarks',
+    lazy = false,
+    config = function()
+        vim.g.bookmark_sign = '⚑'
+        vim.g.bookmark_highlight_group = 'BookmarkLine'
+        vim.g.bookmark_highlight_lines = 1
+        vim.opt.termguicolors = true
+        vim.api.nvim_set_hl(0, 'BookmarkLine', {
+            -- 对于真彩色终端 (termguicolors 开启时):
+            -- 使用十六进制颜色值设置背景和前景
+            bg = '#0087ff', -- 蓝色背景
+            fg = '#000135', -- 黑色前景
+            -- 对于 256 色终端 (termguicolors 关闭时):
+            -- 使用预定义颜色名称设置背景和前景
+            ctermbg = 'blue',  -- 蓝色背景
+            ctermfg = 'black', -- 黑色前景
+            -- 你还可以添加其他属性，例如 'bold = true'
+            -- bold = true,
+        })
+    end,
+}
