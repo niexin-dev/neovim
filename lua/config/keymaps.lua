4357f7c5c735011fff8ec37cd585d2d72ce842cc
Niexin.Liu <383655168@qq.com>
2025-10-11 18:09:46 +0800

    chore(keymaps): 使保存文件快捷键静默
    问题根源：
    保存文件快捷键在执行时会产生命令行输出，可能干扰用户工作流。
    
    解决方案：
    为 `<leader>w` 快捷键的 `vim.keymap.set` 调用添加 `silent = true` 选项。
    
    影响面：
    • 对用户：保存文件操作将不再在命令行显示冗余信息，提升用户体验。
    • 对开发者：无直接影响。
    • 潜在风险：无明显风险。
    
    关键变更点：
    • `lua/config/keymaps.lua` 中 `<leader>w` 快捷键配置添加 `silent = true`。

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,4 +33,4 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent right" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 -- 快速保存
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file", silent = true })
